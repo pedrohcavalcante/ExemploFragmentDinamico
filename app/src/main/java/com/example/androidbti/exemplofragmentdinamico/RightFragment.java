bb2dd56ce030381eff778020e2ac90c707b382bb
Pedro H. B. Cavalcante <pedrohbcavalcante@ufrn.edu.br>
2018-04-10 17:05:06 -0300

    Alteração de dados de edição

--- app/src/main/java/com/example/androidbti/exemplofragmentdinamico/RightFragment.java
+++ app/src/main/java/com/example/androidbti/exemplofragmentdinamico/RightFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by android.bti on 05/04/18.
+ * Created by Pedro H. B. Cavalcante on 05/04/18.
  */
 
 public class RightFragment extends Fragment {
